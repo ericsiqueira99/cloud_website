826bd812b9d3a848557ada9f47686fe6a548a0e2
eric <ericsiqueira99@gmail.com>
2021-06-01 15:31:27 +0100

    fixed path to home from home

--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import static
 
 app_name = 'shop'
 urlpatterns = [
-    path('home', shop.home_view, name='home_page'),
+    path('', shop.home_view, name='home_page'),
     path('login', shop.login_view, name='login_page'),
     path('register', shop.register_view, name='register_page'),
     path('buy/<int:idIten>/', shop.buy_view, name='buy_page'),
