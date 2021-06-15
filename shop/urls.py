5fa19eaf084a5c621b07b5c6fdc00cb0549a5559
Pedro Santos <56020997+a61207@users.noreply.github.com>
2021-06-15 18:52:58 +0100

    Update urls.py
    maybey,aybe

--- shop/urls.py
+++ shop/urls.py
@@ -6,10 +6,10 @@ from django.contrib.staticfiles.urls import static
 app_name = 'shop'
 urlpatterns = [
     path('', shop.home_view, name='home_page'),
-    path('login', shop.login_view, name='login_page'),
-    path('register', shop.register_view, name='register_page'),
-    path('buy/<int:idIten>/', shop.buy_view, name='buy_page'),
-    path('purchase', shop.purchase_view, name='purchase_page'),
-    path('logout', shop.logout_view, name='logout_page'),
+    path('/login', shop.login_view, name='login_page'),
+    path('/register', shop.register_view, name='register_page'),
+    path('/buy/<int:idIten>/', shop.buy_view, name='buy_page'),
+    path('/purchase', shop.purchase_view, name='purchase_page'),
+    path('/logout', shop.logout_view, name='logout_page'),
 
 ]
